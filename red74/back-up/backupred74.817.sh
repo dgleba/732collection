479b99ce2038f1c479e088b81a956a4040bbe48b
David Gleba <dgleba@gmail.com>
2025-11-29 13:14:02 -0500

    2025-11-29a

---
+++ red74/back-up/backupred74.817.sh
@@ -0,0 +1,17 @@
+
+#################################################################
+#@  
+#@   backup key items on red74. some to 4tb and some to koofr
+#@  
+####################################   2025-11-29[Nov-Sat]08-46AM 
+
+# bash /ap/dkr/732collection/red74/back-up/backupred74.817.sh
+# 4 5 * * * bash /ap/dkr/732collection/red74/back-up/backupred74.817.sh
+# * * * * * bash /ap/dkr/732collection/red74/back-up/backupred74.817.sh
+
+
+#crontab
+crontab -l > /am/cruc4tb/koofry/volums/vd70-bakup/bak_red74_ap_dkr/lastbak/crontab-red74.bak
+
+#dkps
+
